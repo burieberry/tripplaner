98bb8fd371b208e1eec7b00a9a7473f5ff9d97d1
Burcu Noyan <burcunoyan@gmail.com>
2017-08-10 21:41:56 -0400

    added Place, Location in Models

--- models/index.js
+++ models/index.js
@@ -4,15 +4,47 @@ const conn = new Sequelize(process.env.DATABASE_URL, {
   logging: false
 });
 
-const Foo = conn.define('foo', {});
-
 const sync = ()=> {
   return conn.sync({ force: true });
 };
 
+const Place = conn.define('place', {
+  address: {
+    type: Sequelize.STRING
+  },
+  city: {
+    type: Sequelize.STRING
+  },
+  state: {
+    type: Sequelize.STRING
+  },
+  phone: {
+    type: Sequelize.STRING
+  },
+  location: {
+    type: Sequelize.ARRAY(Sequelize.FLOAT)
+  }
+});
+
+const Hotel = conn.define('hotel', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  num_stars: {
+    type: Sequelize.FLOAT
+  },
+  amenities: {
+    type: Sequelize.STRING
+  }
+})
+
+
+
 module.exports = {
   sync,
   models: {
-    Foo
+    Place,
+    Hotel
   }
 };
